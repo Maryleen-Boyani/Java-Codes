2972e53489f22f6142fd38e85922bd3d8bf7e5f4
Maryleen Menecha <menechamaryleen@gmail.com>
2023-12-04 19:31:05 +0300

    Last

--- Customer.java
+++ Customer.java
@@ -6,16 +6,18 @@ public class Customer {
     private String firstname;
     private String lastname;
     private int phone;
+    private String address;
 
     public Customer() {
     }
 
-    /**public Customer(String firstname, String lastname, int phone) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.phone = phone;
-    }
-    */
+    /**
+     * public Customer(String firstname, String lastname, int phone) {
+     * this.firstname = firstname;
+     * this.lastname = lastname;
+     * this.phone = phone;
+     * }
+     */
 
     public void getInfo() {
         System.out.println("Enter customers first name:");
@@ -24,11 +26,14 @@ public class Customer {
         lastname = sc.next();
         System.out.println("Enter phone number");
         phone = sc.nextInt();
+        System.out.println("Enter your home address(i.e. P. O. Box 17, Daystar University 90145, Athi-River )");
+        address = sc.next();
         sc.close();
     }
 
     public void displayDetails() {
         System.out.println("Names: " + firstname + " " + lastname);
         System.out.println("Phone: +254" + phone);
+        System.out.println("Address: " + address);
     }
 }
